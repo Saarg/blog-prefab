c36fd4f893a8cc4cae9ea892830cdeabc67fd3c7
saarg <milsonneau.jean@free.fr>
2016-11-18 11:15:28 +0100

    activities routes first draft

--- api/routes/activities.js
+++ api/routes/activities.js
@@ -1,29 +1,84 @@
 'use strict'
 
-var Activity = require('./../models/activities');
+// v1.0 first draft
+
+const Activity = require('./../models/activities');
 
 module.exports = function(router) {
   // activities api route
   router.route('/activities/:page_id')
   .get(function(req, res) {
-    res.json({ message: 'Here will be a list of all the activities available for a page.' });
-  })
+    Activity.find((err, activities) => {
+      if (err) {
+        res.json({ success: false, message: err });
+        return;
+      }
+      res.json({ success: true, activities: activities });
+    });
+  });
 
   // individual media api route
-  router.route('/activity/:activitie_id')
+  router.route('/activity/:activity_id')
   .get(function(req, res) {
-    res.json({ message: 'Not implemented yet.' });
+    Activity.findById(req.params.activitie_id, (err, activity) => {
+      if (err) {
+          res.json({ success: false, message: err });
+          return;
+      }
+      res.json({ success: true, activity: activity });
+    });
   })
 
   .post(function(req, res) {
-    res.json({ message: 'Not implemented yet.' });
+    let activity = new Activity();
+    activity.title = req.body.title;
+    activity.text = req.body.text;
+    activity.page = req.body.page;
+    activity.maxParticipants = typeof req.body.maxParticipants === 'number' ? req.body.maxParticipants : undefined;
+    activity.participants = participants ? participants : undefined;
+    activity.mimetype = req.body.mimetype ? req.body.mimetype : undefined;
+    activity.media = req.body.media ? req.body.media : undefined;
+    activity.position = typeof req.body.position === 'number' ? req.body.position : undefined;
+
+    activity.save((err, activity) => {
+        if (err) {
+            res.json({ success: false, message: err });
+            return;
+        }
+        res.json({ success: true, activity: activity });
+    });
   })
 
   .put(function(req, res) {
-    res.json({ message: 'Not implemented yet.' });
+    Activity.findById(req.params.activity_id, (err, activity) => {
+        if (err) res.send(err);
+        activity.title = req.body.title ? req.body.title : undefined;
+        activity.text = req.body.text ? req.body.text : undefined;
+        activity.page = req.body.page ? req.body.text : undefined;
+        activity.maxParticipants = typeof req.body.maxParticipants === 'number' ? req.body.maxParticipants : undefined;
+        activity.participants = participants ? participants : undefined;
+        activity.mimetype = req.body.mimetype ? req.body.mimetype : undefined;
+        activity.media = req.body.media ? req.body.media : undefined;
+        activity.position = typeof req.body.position === 'number' ? req.body.position : undefined;
+        activity.updated = Date.now();
+
+        activity.save((err) => {
+            if (err) {
+                res.json({ success: false, message: err });
+                return;
+            }
+            res.json({ success: true, activity: activity });
+        });
+    });
   })
 
   .delete(function(req, res) {
-    res.json({ message: 'Not implemented yet.' });
-  })
+    Activity.remove({_id: req.params.activity_id}, (err) => {
+        if (err) {
+            res.json({ success: false, message: err });
+            return;
+        }
+        res.json({ success: true });
+    });
+  });
 }
