8213b93e06af873a60b954c242b30e112f474ab0
Kwarthys <Kwarthys@gmail.com>
2016-11-22 18:28:50 +0100

    Gallery first look
    Added hard-Coded loremPixel image to Gallery. Still can't figure out how to see the center of the image (and not the top-left corner)

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -7,6 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
+  images = [
+    {
+      "mimetype" : "image/png",
+      "media" : "http://lorempixel.com/640/480/"
+    },
+    {
+      "mimetype" : "image/png",
+      "media" : "http://lorempixel.com/500/500/"
+    },
+    {
+      "mimetype" : "image/png",
+      "media" : "http://lorempixel.com/1080/720/"
+    },
+    {
+      "mimetype" : "image/png",
+      "media" : "http://lorempixel.com/350/200/"
+    },
+    {
+      "mimetype" : "image/png",
+      "media" : "http://lorempixel.com/300/300/"
+    },
+    {
+      "mimetype" : "image/png",
+      "media" : "http://lorempixel.com/240/120/"
+    },
+    {
+      "mimetype" : "image/png",
+      "media" : "http://lorempixel.com/666/666/"
+    },
+    {
+      "mimetype" : "image/png",
+      "media" : "http://lorempixel.com/340/340/"
+    },
+    {
+      "mimetype" : "image/png",
+      "media" : "http://lorempixel.com/350/100/"
+    },
+    {
+      "mimetype" : "image/png",
+      "media" : "http://lorempixel.com/600/300/"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
