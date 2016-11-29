0cc0e72a8a9fe186d93245ec161a53193c36cfb2
Kwarthys <Kwarthys@gmail.com>
2016-11-29 18:37:08 +0100

    Many little visual improvement
    displaying number of participants and addition of join in button

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -13,7 +13,7 @@ export class ActivityComponent implements OnInit {
       "text" : "Lorem booling dolor sit amet, sed probo adolescens te. Nec ea solet percipitur. Velit quando commodo sed ut, paulo soluta quaerendum vix ut, id aliquip constituam pri. Ex quo solet molestie. Pertinax hendrerit contentiones ei mea. Eu solet dolorem apeirian his.\
       \
       Ei per porro harum, expetenda cotidieque has at. Error omnes sea an, in nec erat iracundia, eligendi efficiendi pri et. Nam mutat consequat in, sed diam luptatum ad. Id usu novum sonet argumentum. Forensibus quaerendum eam no, erat prima ea mel, id habeo essent graecis nam.",
-      "participants" : ["Roger", "Benoit", "Un débile"]
+      "participants" : ["Roger", "Benoit", "Un débile","Jean"]
     },
     {
       "title" : "Lancer de nain",
@@ -25,13 +25,23 @@ export class ActivityComponent implements OnInit {
     {
       "title" : "Calcul d'intégrale",
       "text" : "Lorem intégrale dolor sit amet, sed probo adolescens te. Nec ea solet percipitur. Velit quando commodo sed ut, paulo soluta quaerendum vix ut, id aliquip constituam pri. Ex quo solet molestie. Pertinax hendrerit contentiones ei mea. Eu solet dolorem apeirian his.",
-      "participants" : ["Un Matheux", "Un autre Matheux", "Encore un autre Matheux"]
+      "participants" : ["Un Matheux", "Un autre Matheux"]
     }
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  joinInButtonCB(i)
+  {
+    var email = prompt("Please enter your Email");
+
+    if(email!=null && email != "")
+    {
+      this.activities[i].participants.push(email);
+    }
   }
 
   goTo(destination) {
